66df8bd97a0a6f95ae90c1413cca8aa2370c845f
leonardo <leonardo.saes16@gmail.com>
2021-07-02 13:07:13 -0300

    :up: Atualizações de contato

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ hamburger.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 })
 
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("js").checked
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    
+    
+    document.querySelector("#preco").innerHTML = `R$ ${preco},00`
+
+
+}
+
+
